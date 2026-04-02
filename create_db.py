09c2ad87eaa766c75e06d8e4d32c05dd3bbe4dd4
Emily Charron <echarron@terpmail.umd.edu>
2026-04-02 16:29:31 -0400

    added new user and copied lecture code

--- create_db.py
+++ create_db.py
@@ -1,4 +1,9 @@
 from app import app, db
+from models import Major, User, Student
+from datetime import datetime as dt
+from werkzeug.security import generate_password_hash
+
+from app import app, db
 from models import Student, Major
 import datetime as dt
 
@@ -15,12 +20,28 @@ with app.app_context():
         db.session.add(a_major)
         db.session.commit()
 
+    # Initial loading of users
+    users = [
+        {'username': 'echarron', 'email': 'echarron@terpmail.umd.edu', 'first_name':'Emily', 'last_name':'Charron',
+            'password': generate_password_hash('echarron', method='pbkdf2:sha256'), 'role':'STUDENT'},
+
+    ]
+
+    for each_user in users:
+        print(f'{each_user["username"]} inserted into user')
+        a_user = User(username=each_user["username"], email=each_user["email"], first_name=each_user["first_name"],
+                    last_name=each_user["last_name"], password=each_user["password"], role=each_user["role"])
+        db.session.add(a_user)
+        db.session.commit()
+
     # Initial loading of students first_name, last_name, major_id, birth_date, is_honors
     students = [
         {'student_id': '1', 'first_name': 'Robert', 'last_name':'Smith', 'email': 'robert.smith@gmail.com', 'major_id':3,
             'birth_date': dt.datetime(2007, 6, 1), 'is_honors':1},
         {'student_id': '2', 'first_name': 'Leo', 'last_name': 'Van Munching', 'email': 'leo.vanmunching@gmail.com', 'major_id':6,
          'birth_date': dt.datetime(2008, 3, 24), 'is_honors': 0},
+        {'student_id':'3', 'first_name':'Emily','last_name':'Charron', 'email':'echarron@terpmail.umd.edu','major_id':3,
+            'birth_date': dt.datetime(2005, 5, 18), 'is_honors': 0}
     ]
 
     for each_student in students:
